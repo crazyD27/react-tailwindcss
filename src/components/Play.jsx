277a7a2f387b561c6a1f6ff48dabb1167ae5403d
Roger-Davis <rd3328425@gmail.com>
2023-06-22 03:50:11 -0300

    feat: Added Play component

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 
+import Title from './Title';
+import Paragraph from './Paragraph';
 import Button from './Button';
 
 const Play = () => {
   return (
     <>
-      <h1>Palavras Secretas</h1>
-      <p>Clique no botão para começar a jogar!</p>
-      <Link to="/new-game">
+      <Title>Palavras Secretas</Title>
+      <Paragraph>Clique no botão para começar a jogar!</Paragraph>
+      <Link to="/game">
         <Button text="Começar jogo" />
       </Link>
     </>
